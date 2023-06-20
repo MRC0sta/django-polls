f569b1c1c7f11ae1355dfd43f14340d4645c51d3
Matheus Costa <matheusrc0107@gmail.com>
2023-06-20 10:48:25 -0300

    Inclui página com as últimas perguntas.

--- polls/urls.py
+++ polls/urls.py
@@ -5,8 +5,11 @@ from . import views
 urlpatterns = [
     path('', views.index, name = "index" ),
     path("sobre/", views.sobre, name="sobre"),
-    path('pergunta/<int:question_id>', views.exibe_questao, name='exibe_questao')
+    path('pergunta/<int:question_id>', views.exibe_questao, name='exibe_questao'),
+    path('perguntas', views.ultimas_perguntas, name='ultimas_perguntas'),
 ]
+
+
 
 # .\venv\Scripts\activate.bat
 # python manage.py runserver
